2a5afc1749f89891b2d7bbc82075a0a199d622d0
Тимур <timurpetrov1875@gmail.com>
2024-03-10 15:18:19 +0300

    third

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,4 +1,4 @@
-create table if not exists TEST(
-    id BIGINT PRIMARY KEY,
-    name varchar(255) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS public.account_types(   id bigint NOT NULL,    name character varying COLLATE pg_catalog."default",    CONSTRAINT account_types_pkey PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS public.users(    id bigint NOT NULL,    fullname character varying(255) COLLATE pg_catalog."default",    total_balance bigint,    CONSTRAINT users_pkey PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS public.settings(   id bigint NOT NULL,    name character varying COLLATE pg_catalog."default",    CONSTRAINT settings_pkey PRIMARY KEY (id));
+
