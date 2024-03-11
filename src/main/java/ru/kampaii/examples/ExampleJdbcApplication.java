3f2adf594abe6a82bf4bc7747237279786958661
Тимур <timurpetrov1875@gmail.com>
2024-03-11 22:01:59 +0300

    fourth

--- src/main/java/ru/kampaii/examples/ExampleJdbcApplication.java
+++ src/main/java/ru/kampaii/examples/ExampleJdbcApplication.java
@@ -2,24 +2,35 @@ package ru.kampaii.examples;
 
 import ru.kampaii.examples.config.DatabaseConnectorProvider;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class ExampleJdbcApplication {
 
+
     public static void main(String[] args) {
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
         try (var connection = DatabaseConnectorProvider.connect();
-             var statement = connection.createStatement()) {
+             var statement = connection.createStatement();
+        ) {
+            InputStream input = ClassLoader.getSystemResourceAsStream("script.sql");
+            Scanner scanner= new Scanner(input);
             System.out.println("Connected succesfully");
-
-            var result = statement.executeUpdate("create table if not exists TEST(\n" +
-                    "    id BIGINT PRIMARY KEY,\n" +
-                    "    name varchar(255) NOT NULL\n" +
-                    ");");
-
+            var result= "";
+            while (scanner.hasNext()) {
+                result += statement.executeUpdate(scanner.nextLine());
+            }
             System.out.println("result is " + result);
 
         } catch (SQLException e) {
             e.printStackTrace();
+
         }
+
+
     }
 }
