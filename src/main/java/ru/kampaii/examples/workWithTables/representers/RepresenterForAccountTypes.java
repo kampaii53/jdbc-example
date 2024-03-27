247919b1c97b027c3cf6105d364b2114b6029ed6
Тимур <timurpetrov1875@gmail.com>
2024-03-27 23:47:43 +0300

    second

--- src/main/java/ru/kampaii/examples/workWithTables/representers/RepresenterForAccountTypes.java
+++ src/main/java/ru/kampaii/examples/workWithTables/representers/RepresenterForAccountTypes.java
@@ -4,63 +4,47 @@ import ru.kampaii.examples.config.DatabaseConnectorProvider;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class RepresenterForAccountTypes {
-    public String getString(int id) {
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        List<String> representList = new ArrayList();
+    public String getString(int id,String columnForSelect) {
+        Map<Integer,String> representMap = new HashMap<>();
         try (var connection = DatabaseConnectorProvider.connect();
              var statement = connection.createStatement();
         ) {
             System.out.println("Connected successfully");
-            int sum=0;
-            var result = statement.executeQuery("SELECT id, name FROM public.account_types;");
-            while (result.next()) {
-                sum++;
-            }
-            for (int i = 0; i < sum+1; i++) {
-                representList.add("");
-            }
             var results = statement.executeQuery("SELECT id, name FROM public.account_types;");
             while (results.next()) {
                 Integer idOfLine = results.getInt(1);
                 String name = results.getString(2);
-                representList.add("id-"+idOfLine+" "+"name-"+name);
+                representMap.put(idOfLine,name);
             }
         } catch (SQLException e) {
             e.printStackTrace();
 
         }
-        return representList.get(id);
+        return representMap.get(id);
     }
-    public List<String> getAllString() {
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        List<String> representList = new ArrayList();
+    public Map<Integer, String> getAllString() {
+        Map<Integer,String> representMap = new HashMap<>();
         try (var connection = DatabaseConnectorProvider.connect();
              var statement = connection.createStatement();
         ) {
             System.out.println("Connected successfully");
-            int sum=0;
-            var result = statement.executeQuery("SELECT id, name FROM public.account_types;");
-            while (result.next()) {
-                sum++;
-            }
-            for (int i = 0; i < sum+1; i++) {
-                representList.add("");
-            }
             var results = statement.executeQuery("SELECT id, name FROM public.account_types;");
             while (results.next()) {
                 Integer idOfLine = results.getInt(1);
                 String name = results.getString(2);
-                representList.add(idOfLine,"id-"+idOfLine+" "+"name-"+name);
+                representMap.put(idOfLine,name);
             }
         } catch (SQLException e) {
             e.printStackTrace();
 
         }
-        return representList;
+        return representMap;
     }
 
 }
